1975df5dda515ba4683ef23c22ef9f8622807f0a
Bo Henriksen <bomarconi@gmail.com>
2018-10-09 18:46:13 +0200

    Vi mangler at konverter secounds og minutes/hours til int. De har mixed datatypes, så vi skal lige finde ud af hvad vi gør.

--- utility/convert_csv.py
+++ utility/convert_csv.py
@@ -15,12 +15,20 @@ def convert_csv_to_dataframe(file_name):
 # , usecols=[0, 2, 3, 7, 8, 9, 10, 14, 15, 16]
 
     # Convert string to datetime
-    # data['release_date'] = pd.to_datetime(data['release_date'], errors='coerce')
+    data['date_time'] = pd.to_datetime(data['date_time'], errors='coerce')
+    data['date_posted'] = pd.to_datetime(data['date_posted'], errors='coerce')
 
-    # Convert string to int. to_numeric converts to float. fillna sets empty values to 0.
-    # data['budget'] = pd.to_numeric(data['budget'], errors='coerce').fillna(0).astype(int)
-    # data['revenue'] = pd.to_numeric(data['revenue'], errors='coerce').fillna(0).astype(int)
-    # data['runtime'] = pd.to_numeric(data['runtime'], errors='coerce').fillna(0).astype(int)
+    # Convert string to int. to_numeric converts to float. fillna sets empty values to 0. .fillna(0)
+    # MIXED DATA TYPES!
+    #data['duration_secounds'] = pd.to_numeric(
+    #    data['duration_secounds'], errors='coerce').astype(int)
+    
+    # MIXED DATA TYPES!
+    # data['duration_minutes'] = pd.to_numeric(data['duration_minutes'], errors='coerce').fillna(0).astype(int)
+    
+    # We use fillna(0) to replace NaN with 0 so we can convert to int.
+    data['latitude'] = pd.to_numeric(data['latitude'], errors='coerce').fillna(0).astype(int)  
+    data['longitude'] = pd.to_numeric(data['longitude'], errors='coerce').fillna(0).astype(int) 
 
     # Convert string to int and replace .
     # data['popularity'] = pd.to_numeric(data['popularity'].str.replace(
