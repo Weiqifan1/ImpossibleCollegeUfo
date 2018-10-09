20fc6a3b7eeae8851fd53ee48c40c6af89ba4b9a
Weiqifan1 <cmlykke@hotmail.com>
2018-10-09 13:10:08 +0200

    tilføjet downloader og convert_csv.py

---
+++ lib/convert_csv.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def convert_csv_to_dataframe(file_name):
+    '''
+    This method read the csv file and convert it to a dataframe. 
+    When it's done it change the date to a date time object and some of the columns to integers.
+    '''
+    # Returneres som en dataframe. 
+    # low_memory=False because column 10 has mixed datatypes.
+    data = pd.read_csv(file_name, sep=',', usecols=[0, 2, 3, 7, 8, 9, 10, 14, 15, 16], low_memory=False) 
+    
+    # Convert string to datetime
+    data['release_date'] = pd.to_datetime(data['release_date'], errors='coerce')
+
+    # Convert string to int. to_numeric converts to float. fillna sets empty values to 0.
+    data['budget'] = pd.to_numeric(data['budget'], errors='coerce').fillna(0).astype(int)
+    data['revenue'] = pd.to_numeric(data['revenue'], errors='coerce').fillna(0).astype(int)
+    data['runtime'] = pd.to_numeric(data['runtime'], errors='coerce').fillna(0).astype(int)
+
+    # Convert string to int and replace .
+    data['popularity'] = pd.to_numeric(data['popularity'].str.replace(
+        '.', ''), errors='coerce').fillna(0).astype(int)
+
+    return data
---
+++ lib/downloader.py
@@ -0,0 +1,48 @@
+
+# download the file and change it to a panda dataset
+
+"""
+Downloads a file and returns it to main.py.
+Usage:
+    python main.py [<url>] 
+    or
+    python main.py [<url>] [<file_name>]
+"""
+
+import os
+import sys
+from urllib import request as req
+
+
+def download(from_url, to_file):
+    if not os.path.isfile(to_file):
+        req.urlretrieve(from_url, to_file)
+
+
+def download_file():
+    try:
+        global file_name
+        _, url, file_name = sys.argv
+    except:
+        try:
+            _, url = sys.argv
+            file_name = os.path.basename(url)
+        except:
+
+            try:
+
+                # open file
+                cfg_file = file_name
+                with open(cfg_file) as fp:
+                    for line in fp:
+
+                        file_name = os.path.basename(line.rstrip())
+                        url = line
+                        download(url, file_name)
+                sys.exit(0)
+            except Exception as e:
+                print(__doc__)
+                sys.exit(1)
+
+    download(url, file_name)
+    return file_name
