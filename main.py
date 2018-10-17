69746f349cab3c7ee1df9abd240ab9f36a727f19
Weiqifan1 <cmlykke@hotmail.com>
2018-10-17 17:44:49 +0200

    added a file to calculate average length of a ufo sighting

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import library.sightings_over_time as sightings
 import library.most_ufo_sightings as sights
 import library.most_sightings_monthly as monthly_sightings
 import library.ufolook as look
+import library.minutes as sight_min
 
 # Udkommenteret mens vi udvikler
 """ if __name__ == '__main__':
@@ -22,11 +23,12 @@ file_name = 'ufo-scrubbed-geocoded-time-standardized.csv'
 
 data = convert_csv.convert_csv_to_dataframe(file_name)
 
-#2018-10-17: disse 3 linjer skal udkommenteres
+#2018-10-17: disse 4 linjer skal indkommenteres inden vi merger til master
     #print(sightings.ufo_sightings_over_time(data))
     #print(monthly_sightings.ufo_sightings_monthly(data))
     #sights.find_state(data)
-look.ufolook(data)
+    #look.ufolook(data)   # note, der er endnu ikke lavet textblob analyse
+sight_min.ufo_minutes(data)
 print("end")
 
 # Gør at vi kan printe all kolonner.
