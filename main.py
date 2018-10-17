141a4acca57d6ba6f4c85353da6200fa258ac70d
Weiqifan1 <cmlykke@hotmail.com>
2018-10-17 19:46:01 +0200

    added weekdays.py = creates a pie chart of each weekday

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import library.most_ufo_sightings as sights
 import library.most_sightings_monthly as monthly_sightings
 import library.ufolook as look
 import library.minutes as sight_min
+import library.weekdays as week
 
 # Udkommenteret mens vi udvikler
 """ if __name__ == '__main__':
@@ -23,13 +24,14 @@ file_name = 'ufo-scrubbed-geocoded-time-standardized.csv'
 
 data = convert_csv.convert_csv_to_dataframe(file_name)
 
-#2018-10-17: disse 4 linjer skal indkommenteres inden vi merger til master
-    #print(sightings.ufo_sightings_over_time(data))
-    #print(monthly_sightings.ufo_sightings_monthly(data))
-    #sights.find_state(data)
-    #look.ufolook(data)   # note, der er endnu ikke lavet textblob analyse
+print(sightings.ufo_sightings_over_time(data))
+print(monthly_sightings.ufo_sightings_monthly(data))
+sights.find_state(data)
+look.ufolook(data)   # note, der er endnu ikke lavet textblob analyse
 sight_min.ufo_minutes(data)
+week.week_graf(data)
 print("end")
+
 
 # Gør at vi kan printe all kolonner.
 #pd.set_option('display.max_columns', None)
